039a260bd132debdc4300f0b0231495ea0642cfb
dutao <dutao1992@gmail.com>
2016-12-13 22:18:16 +0800

    Update Symmetric_Difference.js

--- Symmetric_Difference.js
+++ Symmetric_Difference.js
@@ -1,3 +1,4 @@
+//该函数返回数组中唯一元素的集合
 function unique(array){
   var n = []; //一个新的临时数组
   for(var i = 0; i < array.length; i++){
@@ -5,7 +6,7 @@ function unique(array){
   }
   return n;
 }
-
+//该函数返回两个数组中唯一元素组成的数组
 function sd(arr1,arr2){
   arr1 = unique(arr1);
   arr2 = unique(arr2);
@@ -19,6 +20,7 @@ function sd(arr1,arr2){
   return unique_arr.sort();
 }
 
+//主函数，对任意多个数组进行对等差分操作，返回最后的数组
 function sym(arg) {
   var args = Array.prototype.slice.call(arguments, 0);//调用所有的参数
   return args.reduce(sd);
